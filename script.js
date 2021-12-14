4934992e6a51a4784b2ae8877ea30173258bf563
iMac_02 <imac_02@192.168.50.21>
2021-12-14 12:48:34 +0000

    deuxieme commit

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-btn_inscription.onclick = () =>{
-    localStorage.setItem()
+let bouton = document.querySelector('.btn-inscription')
+
+bouton.onclick = () =>{
+    let data ={
+        prenom: document.getElementById('lastname').value,
+        name_user: document.getElementById('name_user').value,
+        password :document.getElementById('password').value,
+        mail : document.getElementById('mail').value
+    }
+    console.log("data",data);
+
+    localStorage.setItem("user_data",JSON.stringify(data) );
+   
 }
+
+
+
+
+
