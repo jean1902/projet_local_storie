c4f7bc7d093e27575922245c474f7543e84b1a97
jean1902 <cin71781@gmail.com>
2021-12-15 12:35:11 +0100

    commit_image_bg

--- exercice_jour1/script.js
+++ exercice_jour1/script.js
@@ -1,10 +1,26 @@
-let bouton = document.querySelector('.btn-inscription')
+let bouton = document.querySelector('.formulaire_inscription');
 
-bouton.onclick = () =>{
-    localStorage.setItem("name",name.value);
-    localStorage.setItem("lastname",lastname.value);
-    localStorage.setItem("name_user",name_user.value);
-    localStorage.setItem("password",password.value);
-    localStorage.setItem("mail",mail.value);
-    localStorage.setItem("check",check.value);
+let data ={
+    name :name.value,
+    lastname :lastname.value,
+    name_user : name_user.value,
+    password :password.value,
+    mail :mail.value 
 }
+
+bouton.addEventListener("submit",()=>{
+
+    localStorage.setItem("name",name.value);
+
+})
+
+// bouton.onclick = () =>{
+//     localStorage.setItem("name",name.value);
+//     localStorage.setItem("lastname",lastname.value);
+//     localStorage.setItem("name_user",name_user.value);
+//     localStorage.setItem("password",password.value);
+//     localStorage.setItem("mail",mail.value);
+//     localStorage.setItem("check",check.value);
+// }
+
+
